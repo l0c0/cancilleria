9e1e9efbc79360dc2aae49ad926e550ba60bd464
Andres Grajales <ananes77@hotmail.com>
2013-10-04 14:13:45 -0500

    Cambio en parametros del sistema

---
+++ sites/default/modules/cancille/js/cancille.js
@@ -0,0 +1,20 @@
+(function ($) {
+
+  $(document).ready(function(){
+    $("#edit-pais").change(function(event) {
+      $('.form-item-tipo').css('display','block');
+    });
+
+    $("#edit-tipo").change(function(event) {
+//      alert($("#edit-tipo").val());
+      if ($("#edit-tipo").val() > 0) {
+	  $.get(Drupal.t('/cancille/@nid', {'@nid': $("#edit-tipo").val()}),
+	  function(data){
+		$("#markup-node").html(data);
+      });
+      }
+    });
+
+  });
+
+})(jQuery);
